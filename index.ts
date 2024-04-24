0ffa7f513ce9aeb6c651667e073a571afe187f5b
rimsha1994 <khanzadimaryam27@gmail.com>
2024-04-25 00:25:02 +0500

    push my project number guess

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+let guessedCorrectly = false;
+
+while (!guessedCorrectly) {
+    const answers = await inquirer.prompt([
+        { name: "userGuessedNumber", type: "number", message: chalk.green("Please guess a number between 1-10") },
+    ]);
+
+    if (answers.userGuessedNumber === randomNumber) {
+        console.log(chalk.yellow.bold("Congratulations! You got it right!"));
+        guessedCorrectly = true;
+    } else {
+        console.log(chalk.redBright("You guessed the wrong number. Try again!"));
+    }
+}
